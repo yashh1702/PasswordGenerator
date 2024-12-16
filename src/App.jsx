dcce531c9aace667c0c9a43e62e4a18af3e3f8cb
Yashagrawal1702 <yash1702agrawal@gmail.com>
2024-12-16 23:55:25 +0530

    use of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -31,13 +31,15 @@ function App() {
     // Update button text to 'Copied'
     setButtonText('Copied');
     
-    // Reset button text after 2 seconds
-    setTimeout(() => setButtonText('Copy'), 2000);
   }, [password]);
 
   useEffect(() => {
     passwordGenerator();
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  useEffect(() =>{
+    setButtonText('Copy');
+  },[password]);
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
